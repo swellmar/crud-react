6a138b643af4335ffb024ea91d4a948393649700
Sebastian Wellmar <sebastian.wellmar@chas.se>
2017-01-31 23:14:14 +0100

    Adding Edit and Delete Buttons etc

--- src/actions.js
+++ src/actions.js
@@ -3,61 +3,61 @@ export const ADD_GAME = 'ADD_GAME';
 export const GAME_FETCHED = 'GAME_FETCHED';
 
 function handleResponse(response) {
-	if (response.ok) {
-		return response.json();
-	} else {
-		let error = new Error(response.statusText);
-		error.response = response;
-		throw error;
-	}
+  if (response.ok) {
+    return response.json();
+  } else {
+    let error = new Error(response.statusText);
+    error.response = response;
+    throw error;
+  }
 }
+
 export function setGames(games) {
-	return {
-		type: SET_GAMES,
-		games
-	}
+  return {
+    type: SET_GAMES,
+    games
+  }
 }
 
 export function addGame(game) {
-	return {
-		type: ADD_GAME,
-		game
-	}
+  return {
+    type: ADD_GAME,
+    game
+  }
 }
 
 export function gameFetched(game) {
-	return {
-		type: GAME_FETCHED,
-		game
-	}
+  return {
+    type: GAME_FETCHED,
+    game
+  }
 }
 
-
 export function saveGame(data) {
-	return dispatch => {
-		return fetch('/api/games', {
-			method: 'post',
-			body: JSON.stringify(data),
-			headers: {
-				"Content-Type": "application/json"
-			}
-		}).then(handleResponse)
-		.then(data => dispatch(addGame(data.game)));
-	}
+  return dispatch => {
+    return fetch('/api/games', {
+      method: 'post',
+      body: JSON.stringify(data),
+      headers: {
+        "Content-Type": "application/json"
+      }
+    }).then(handleResponse)
+    .then(data => dispatch(addGame(data.game)));
+  }
 }
 
 export function fetchGames() {
-return dispatch => {
-	fetch('/api/games')
-		.then(res => res.json())
-		.then(data => dispatch(setGames(data.games)));
-}
+  return dispatch => {
+    fetch('/api/games')
+      .then(res => res.json())
+      .then(data => dispatch(setGames(data.games)));
+  }
 }
 
 export function fetchGame(id) {
-return dispatch => {
-	fetch('/api/games/${id}')
-		.then(res => res.json())
-		.then(data => dispatch(gameFetched(data.game)));
-}
+  return dispatch => {
+    fetch(`/api/games/${id}`)
+      .then(res => res.json())
+      .then(data => dispatch(gameFetched(data.game)));
+  }
 }
